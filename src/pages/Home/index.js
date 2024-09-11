9183a44e27daecc22a800f7bd0f2b317c4520c7c
Rama krishna <ramakrishnagarlapati04@gmail.com>
2024-09-11 09:50:37 +0530

    Implement Home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,25 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
 
-import "./index.js";
-
-const Home = () => {
-  return <div>Home</div>;
-};
+function Home() {
+  return (
+    <div className="home-page-container">
+      <div className="home-container">
+        <div className="home-content">
+          <h1 className="home-heading">Find The Job That Fits Your Life</h1>
+          <p className="home-description">
+            Millions of people are searching for jobs, salary information,
+            company reviews. Find the job that fits your ability and potential.
+          </p>
+          <Link to="/jobs">
+            <button className="find-jobs-btn" type="button">
+              Find Jobs
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Home;
